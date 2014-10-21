ed45444a958ad42216e58c9a0c01953cd24e5e27
gsvr <george@westerncapelabs.com>
2014-10-21 16:50:09 +0200

    copy and adjust from serviceratingstore

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-messaging-subscription",
+    version="0.0.1a",
+    url="https://github.com/westerncapelabs/django-messaging-subscription",
+    license='BSD',
+    description=(
+        "A RESTful API for managing messaging content, subscriptions and sending via Vumi-go"),
+    long_description=open('README.rst', 'r').read(),
+    author='Western Cape Labs',
+    author_email='devops@westerncapelabs.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Django',
+        'django-tastypie',
+        'South',
+        'gunicorn==19.0.0',
+        'django-celery==3.1.10',
+        'go-http==0.1.1',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking',
+    ],
+)
---
+++ testsettings.py
@@ -0,0 +1,37 @@
+DEBUG = True
+
+SECRET_KEY = 'ItsSekret'
+
+USE_TZ = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'subscriptionstore.db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    # Third-party apps
+    'south',
+    'tastypie',
+    # Us
+    'subscription'
+]
+
+VUMI_GO_ACCOUNT_KEY = "replaceme"
+VUMI_GO_CONVERSATION_KEY = "replaceme"
+VUMI_GO_ACCOUNT_TOKEN = "replaceme"
+
+ROOT_URLCONF = 'subscription.urls'
