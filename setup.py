b5c474aad9b1cdfb5c77507f8ccd47bdbe0eb8d5
Mike Jones <mike@westerncapelabs.com>
2014-10-22 16:10:38 +0200

    Bump version to 0.1.0a post-release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-messaging-subscription",
-    version="0.1.0",
+    version="0.1.0a",
     url="https://github.com/westerncapelabs/django-messaging-subscription",
     license='BSD',
     description=(
