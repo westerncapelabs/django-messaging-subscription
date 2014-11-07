b24cbaed0cd57b821d3b612f178b64272723ab76
Mike Jones <mike@westerncapelabs.com>
2014-11-07 12:45:59 +0200

    Release 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-messaging-subscription",
-    version="0.3.0a",
+    version="0.3.1",
     url="https://github.com/westerncapelabs/django-messaging-subscription",
     license='BSD',
     description=(
